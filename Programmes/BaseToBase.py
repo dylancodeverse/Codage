23dd5fb2849e915e3ff465b7ad6cb56c66368ffa
dylancodeverse <ratianarivodylan@gmail.com>
2024-04-10 13:16:43 +0300

    passant par la base 10

--- Programmes/BaseToBase.py
+++ Programmes/BaseToBase.py
@@ -1,18 +1,16 @@
 def base10EnAutreBase (basecible:int , nombre:int , dictionnaire :dict):
     # division successive
     reste = []
-    nombre= int(nombre / basecible)
-    while nombre >= basecible :
-        if dictionnaire.get(str (nombre%basecible)) != None :
+    while nombre !=0 :
+        if dictionnaire !=None and dictionnaire.get(str (nombre%basecible)) != None :
             reste.insert(0,dictionnaire.get(str(nombre%basecible)))
         else:    
             reste.insert(0,str(nombre%basecible))
         nombre= int (nombre /basecible)
 
-    reste.insert(0,str (nombre))
     return ''.join(reste)
 
-# print(base10EnAutreBase(2,35)) 
+# print(base10EnAutreBase(8,81,None)) 
 
 
 # mbola tsy mazaka anle misy lettre
@@ -28,7 +26,7 @@ def enBase10(nombre:str, baseDOrigine:int , dictionnaire :dict ):
         i+=1
     return resultat
 
-# print(enBase10('AB',16, {'A':10, 'B':11, 'C':12 ,'D':13, 'E':14, 'F':15 } )) 
+# print(enBase10('1010001',2, {'A':10, 'B':11, 'C':12 ,'D':13, 'E':14, 'F':15 } )) 
 
 
 def binaireEnHexadecimalMethodeBloc (nombre:str, dictionnaire = {'A':10, 'B':11, 'C':12 ,'D':13, 'E':14, 'F':15 } ):    
@@ -59,3 +57,8 @@ def binaireEnOctalMethodeBloc (nombre:str):
 print(binaireEnOctalMethodeBloc('1010001'))
 
 
+def baseToBasePassantParLaBase10 (nombre :str , baseDorigine:int, dictionnaireBaseDorigine :dict , baseCible:int, dictionnaireBaseCible:dict):
+    nombreEnBase10 = enBase10(nombre,baseDorigine,dictionnaireBaseDorigine)
+    return base10EnAutreBase(baseCible,nombreEnBase10, dictionnaireBaseCible)
+
+print(baseToBasePassantParLaBase10('1010001' , 2 ,None , 8 ,None))
