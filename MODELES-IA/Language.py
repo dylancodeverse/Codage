f6a34510b10d6f8b2d1e2e171fec218db623ddb9
dylancodeverse <ratianarivodylan@gmail.com>
2024-05-22 15:32:13 +0300

    datas prepared

--- MODELES-IA/Language.py
+++ MODELES-IA/Language.py
@@ -1,6 +1,10 @@
 
 import SimilarityAnalysis
 import ast
+import time
+import csv
+
+
 class Language :
     def __init__(self , isCode:bool, language:list[str]) -> None:
         self.isCode = isCode
@@ -10,9 +14,57 @@ class Language :
         self.setAVGDiffRatio(language)
         self.setAVGDistancedeLevenshtein(language)
         self.setAVGSimilarityendstart(language)
+        self.label = str(language)
 
     @staticmethod
-    def createLanguagesFromData(codePath , notCodePath)->list['Language']:
+    def exportDataAsCSV(exportedCode ,exportedNotCode , codePath,notCodePath):
+        fsec= (time.time())
+        print('first step loading....')
+        one ,two =Language.createLanguagesFromData(codePath, notCodePath)
+        print(time.time()-fsec)
+        lan = [one , two]
+        print('data prepared')
+
+        exp = [exportedCode, exportedNotCode ]
+
+        for i in range(len(lan)) :
+            header =[   
+                        "label",
+                        "isCode",
+                        "wordsLen",
+                        "ZeroLen",
+                        "OneLen",
+                        "WordsWithSameSizeLen",
+                        "AVGWordsSize",
+                        "AVGDiffRatio",
+                        "AVGDistancedeLevenshtein",
+                        "AVGSimilarityendstart"
+                    ]
+            fsec= (time.time())
+            print('export csv....')
+            with open(exp[i],'w',newline='') as filee:
+                writer= csv.DictWriter(filee,fieldnames=header)
+                for langg in lan [i] :
+                    writer.writerow(
+                    {
+                        "label":langg.label
+                        ,"isCode":langg.isCode
+                        ,"wordsLen":langg.wordsLen
+                        ,"ZeroLen":langg.zeroLen
+                        ,"OneLen":langg.oneLen
+                        ,"WordsWithSameSizeLen":langg.wordsWithSameSizeLen
+                        ,"AVGWordsSize":langg.AVGWordsSize
+                        ,"AVGDiffRatio":langg.AVGDiffRatio
+                        ,"AVGDistancedeLevenshtein":langg.AVGDistancedeLevenshtein
+                        ,"AVGSimilarityendstart":langg.AVGSimilarityendstart
+                    })
+            print(time.time()-fsec)
+
+
+
+
+    @staticmethod
+    def createLanguagesFromData(codePath , notCodePath):
         listCodes =[]
         listNotCodes = []
         with open(codePath ,'r') as codeFile , open(notCodePath ,'r') as notCodeFile:
@@ -28,6 +80,7 @@ class Language :
             listLanguageCodes.append(Language(True,code))
         for notCode in listNotCodes :
             listLanguageNotCodes.append(Language(False,notCode)) 
+        return listLanguageCodes,listLanguageNotCodes            
 
     def setZeroLen_OneLen (self , language:list[str]):
         self.zeroLen =  0
@@ -40,14 +93,14 @@ class Language :
                     self.oneLen+=1
 
     def setWordsWithSameSizeLen_AVGWordsSize (self , language):
-        self.ordsWithSameSizeLen = 0
+        self.wordsWithSameSizeLen = 0
         self.AVGWordsSize = 0
         sizeWord = []
         for i in range(0,len(language)):
             sizeWord.append(len(language[i]))
             for j in range(i+1,len(language)):
                 if len(language[i]) == len(language[j]):
-                     self.ordsWithSameSizeLen+=1
+                     self.wordsWithSameSizeLen+=1
         self.AVGWordsSize = sum(sizeWord)/len(language)            
 
     def setAVGDiffRatio(self,language ):
@@ -62,4 +115,10 @@ class Language :
 
 codePath = "C:/Users/MISA/Desktop/Workspace/S6/Codage/MODELES-IA/datas/codeDatas.txt"
 notCodePath = "C:/Users/MISA/Desktop/Workspace/S6/Codage/MODELES-IA/datas/notCodeDatas.txt"
-Language.createLanguagesFromData(codePath, notCodePath)
+expcodePath = "C:/Users/MISA/Desktop/Workspace/S6/Codage/MODELES-IA/datas/dataCode.csv"
+expnotCodePath = "C:/Users/MISA/Desktop/Workspace/S6/Codage/MODELES-IA/datas/datanotCode.csv"
+
+Language.exportDataAsCSV(codePath=codePath ,exportedCode=expcodePath,exportedNotCode=expnotCodePath,notCodePath=notCodePath)
+
+
+
